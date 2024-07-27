f65a7dccb7925dc49cdc4752ea109c9f248f5e94
saac03 <isaac.dicayanan@gmail.com>
2024-07-28 03:29:46 +0800

    dropdown functionality and dynamic rendering with dummy data in dashboard

--- client/src/Dashboard/Dashboard.js
+++ client/src/Dashboard/Dashboard.js
@@ -1,40 +1,59 @@
 import Sidebar from '../Sidebar/Sidebar.js'
 import styles from './Dashboard.module.css'
 
+import { useState } from 'react'
+
+// temporary dummy database entries
+const items = [
+    { id: 'id124', label: 'ID124', list: ['BS Interactive Entertainment major in Game Design', 'BS Interactive Entertainment major in Game Development', 'BS Information Systems', 'BS Computer Science major in Software Technology'] },
+    { id: 'id123', label: 'ID123', list: ['BS Interactive Entertainment major in Game Design', 'BS Interactive Entertainment major in Game Development', 'BS Information Systems', 'BS Computer Science major in Software Technology'] }
+];
+
 function Dashboard() {
 
-    return(
-        <body>
+    const initialDropdownState = items.reduce((acc, item) => {
+        acc[item.id] = false;
+        return acc;
+    }, {});
 
-        
+    const [isDropdownOpen, setIsDropdownOpen] = useState(initialDropdownState);
+
+    const toggleDropdown = (id) => {
+        setIsDropdownOpen(prevState => ({
+            ...prevState,
+            [id]: !prevState[id]
+        }));
+    };
+
+    const programList = items.map(item => (
+        <div className={styles.item} key={item.id}>
+            <div className={styles.itemHeader} onClick={() => toggleDropdown(item.id)}>
+                <div className={styles.dropdownIcon}><img src='/img/icons/dropdown.png' alt="dropdown icon"/></div>
+                <div>{item.label}</div>
+            </div>
+            {isDropdownOpen[item.id] && (
+                 <ul className={`${styles.itemList} ${isDropdownOpen[item.id] ? styles.open : ''}`}>
+                    {item.list.map((programName, index) => (
+                        <li key={index}>{programName}</li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    ))
+
+    
+    return(
+
         <div className={styles.container}>
             <Sidebar/>
             <div className={styles.content}>
                 <div className={styles.head}>
                     <p> CCS </p>
                 </div>
-                <div className={styles.item}>
-                    <div className={styles.itemHeader} id="">ID124</div>
-                    <ul className={styles.itemList}>
-                        <li>BS Interactive Entertainment major in Game Design</li>
-                        <li>BS Interactive Entertainment major in Game Development</li>
-                        <li>BS Information Systems</li>
-                        <li>BS Computer Science major in Software Technology</li>
-                    </ul>
-                </div>
-                <div className={styles.item}>
-                    <div className={styles.itemHeader} id="">ID123</div>
-                    <ul className={styles.itemList}>
-                        <li>BS Interactive Entertainment major in Game Design</li>
-                        <li>BS Interactive Entertainment major in Game Development</li>
-                        <li>BS Information Systems</li>
-                        <li>BS Computer Science major in Software Technology</li>
-                    </ul>
-                </div>
-
+                {programList}
             </div>
         </div>
-        </body>
+
     );
 
 }
