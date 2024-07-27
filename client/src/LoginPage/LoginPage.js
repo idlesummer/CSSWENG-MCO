95e946fcdfbeea9a225366540b3da0ce0b9853ed
saac03 <isaac.dicayanan@gmail.com>
2024-07-28 02:23:39 +0800

    Login Page router

--- client/src/LoginPage/LoginPage.js
+++ client/src/LoginPage/LoginPage.js
@@ -1,13 +1,22 @@
 import styles from './LoginPage.module.css'
+import { useNavigate } from 'react-router-dom';
+
 
 function LoginPage() {
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        navigate("/");
+    }
 
     return(
             <div className={styles.container}>
                 <div className={styles.welcomeSection}>
                     <h1>Hello, </h1>
                     <h1>welcome!</h1>
-                    <form className={styles.loginForm}>
+                    <form className={styles.loginForm} onSubmit={e => handleSubmit(e)}>
                         <label for="email">Email</label>
                         <input type="email" id="email" placeholder="Enter your email address" required/>
                         <label for="password">Password</label>
