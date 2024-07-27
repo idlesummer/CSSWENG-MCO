eba31e2c071cf407ef5575d7b2b3f0099e36278f
saac03 <isaac.dicayanan@gmail.com>
2024-07-28 00:21:21 +0800

    Dynamic CoursePage Rendering with dummy data
    object array used for CoursePage rendering is in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,51 @@
 import './App.css';
 // import Dashboard from './Dashboard/Dashboard.js'
 import LoginPage from './LoginPage/LoginPage.js'
-// import CoursePage from './CoursePage/CoursePage.js'
+import CoursePage from './CoursePage/CoursePage.js'
 
 
 
 function App() {
+
+  const sampleClass = [
+    { id: 1,
+    code: "GEFTWEL", 
+    title: "Physical Fitness and Wellness", 
+    offered_to: "X", 
+    section: "X22",
+    faculty: "TUMALE, MARIA CRISTINA", 
+    day1: "T", 
+    begin1: "1300", 
+    end1: "1500", 
+    room1: "Gym", 
+    day2: "F",
+    begin2: "1300",
+    end2: "1500", 
+    room2: "Gym", 
+    enrl_cap: "40",
+    remarks: "Remark Sample",
+  },
+  { id: 2,
+    code: "SAS", 
+    title: "Sas Title", 
+    offered_to: "X", 
+    section: "X22",
+    faculty: "TUMALE, MARIA CRISTINA", 
+    day1: "T", 
+    begin1: "1300", 
+    end1: "1500", 
+    room1: "Gym", 
+    day2: "F",
+    begin2: "1300",
+    end2: "1500", 
+    room2: "Gym", 
+    enrl_cap: "40",
+    remarks: "Remark Sample",
+  }    
+];
+
   return (
-    <LoginPage />
+    <CoursePage courseList={sampleClass} />
   );
 }
 
--- client/src/CoursePage/CoursePage.js
+++ client/src/CoursePage/CoursePage.js
@@ -1,21 +1,42 @@
 import Sidebar from '../Sidebar/Sidebar.js'
 import styles from './CoursePage.module.css'
 
-function CoursePage(){
+import PropTypes from 'prop-types'
 
+function CoursePage({ courseList }){
+
+    const courses = courseList.map((course, index) => (
+        <tr key={index}>
+          <td><input type="checkbox" /></td>
+          <td>{course.code}</td>
+          <td>{course.title}</td>
+          <td>{course.offered_to}</td>
+          <td>{course.section}</td>
+          <td>{course.faculty}</td>
+          <td>{course.day1}</td>
+          <td>{course.begin1}</td>
+          <td>{course.end1}</td>
+          <td>{course.room1}</td>
+          <td>{course.day2}</td>
+          <td>{course.begin2}</td>
+          <td>{course.end2}</td>
+          <td>{course.room2}</td>
+          <td>{course.enrl_cap}</td>
+          <td>{course.remarks}</td>
+        </tr>
+      ));
+        
     return(
-        <body>
-
         <div className={styles.container}>
             <Sidebar/>
-            <div class={styles.content}>
+            <div className={styles.content}>
                 <h1>ID124 / BSIET-AD</h1>
-                <div class={styles.tableWrapper}>
-                    <div class={styles.controls}>
+                <div className={styles.tableWrapper}>
+                    <div className={styles.controls}>
                         <div className={styles.searchBar}>
                             <input type="text"/>
                         </div>
-                        <div class={styles.iconButtons}>
+                        <div className={styles.iconButtons}>
                             <div className={`${styles.iconButton} ${styles.addIcon}`}></div>
                             <div className={`${styles.iconButton} ${styles.editIcon}`}></div>
                             <div className={`${styles.iconButton} ${styles.deleteIcon}`}></div>
@@ -31,55 +52,75 @@ function CoursePage(){
                                 <th>Offered To</th>
                                 <th>Sect</th>
                                 <th>Faculty</th>
-                                <th>Day1</th>
-                                <th>Begin1</th>
-                                <th>End1</th>
-                                <th>Day2</th>
-                                <th>Begin2</th>
-                                <th>End2</th>
-                                <th>Room</th>
+                                <th>Day 1</th>
+                                <th>Begin 1</th>
+                                <th>End 1</th> 
+                                <th>Room 1</th> 
+                                <th>Day 2</th>
+                                <th>Begin 2</th>
+                                <th>End 2</th> 
+                                <th>Room 2</th> 
+                                <th>Enrll Cap</th> 
+                                <th>Remarks</th> 
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td><input type="checkbox"/></td>
-                                <td>GEFTWEL</td>
-                                <td>Physical Fitness and Wellness</td>
-                                <td>X</td>
-                                <td>X22</td>
-                                <td>TUMALE, MARIA CRISTINA</td>
-                                <td>T</td>
-                                <td>1300</td>
-                                <td>1500</td>
-                                <td>F</td>
-                                <td>1300</td>
-                                <td>1500</td>
-                                <td>Gym</td>
-                            </tr>
-                            <tr>
-                                <td><input type="checkbox"/></td>
-                                <td>SAS1000</td>
-                                <td>Student Affairs Services 1000</td>
-                                <td>X</td>
-                                <td>X23</td>
-                                <td>TABA, PATRICIA MAE</td>
-                                <td>10-May-23</td>
-                                <td>1030</td>
-                                <td>1230</td>
-                                <td>W</td>
-                                <td>1030</td>
-                                <td>1230</td>
-                                <td>Room 101</td>
-                            </tr>
+                            {courses}
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
-        </body>
 
     );
 
 }
+
+CoursePage.propTypes = {
+    courseList: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        code: PropTypes.string,
+        title: PropTypes.string,
+        offered_to: PropTypes.string,
+        section: PropTypes.string,
+        faculty: PropTypes.string,
+        day1: PropTypes.string,
+        begin1: PropTypes.string,
+        end1: PropTypes.string,
+        room1: PropTypes.string,
+        day2: PropTypes.string,
+        begin2: PropTypes.string,
+        end2: PropTypes.string,
+        room2: PropTypes.string,
+        enrl_cap: PropTypes.string,
+        remarks: PropTypes.string
+      })
+    )
+  };
+  
+// not working
+CoursePage.defaultProps = {
+    courseList: [
+      {
+        id: 0,
+        code: "N/A",
+        title: "No Data",
+        offered_to: "N/A",
+        section: "N/A",
+        faculty: "N/A",
+        day1: "N/A",
+        begin1: "N/A",
+        end1: "N/A",
+        room1: "N/A",
+        day2: "N/A",
+        begin2: "N/A",
+        end2: "N/A",
+        room2: "N/A",
+        enrl_cap: "0",
+        remarks: "No Remarks"
+      }
+    ]
+  };
 
 export default CoursePage
