44da687bb2010484cbe27a728181c207bce8b79c
Nash Luis Maramag <09louisthebob@gmail.com>
2024-07-29 03:26:11 +0800

    Update CourseOfferings.js

--- frontend/src/CourseOfferings/CourseOfferings.js
+++ frontend/src/CourseOfferings/CourseOfferings.js
@@ -50,60 +50,58 @@ function CourseOfferings({ courseList , takersList}){
     const [openAddModal, setOpenAddModal] = useState(false)
 
     return (
-        <div className={styles.container}>
-            <Sidebar/>
-            <div className={styles.content}>
-                <h1>SIS Course Offerings for T3</h1>
-                <div className={styles.tableWrapper}>
-                    <div className={styles.controls}>
-                        <div className={styles.searchBar}>
-                            <input type="text"/>
-                        </div>
-                        <div className={styles.iconButtons}>
-                            <div className={`${styles.iconButton} ${styles.addIcon}`}onClick={() => {setOpenAddModal(true)}}><img src="/img/icons/plus.png" alt="add"></img></div>
-                            {openAddModal && <AddModal closeModal={setOpenAddModal}/>}
-                            <div className={`${styles.iconButton} ${styles.editIcon}`} onClick={() => {setOpenEditModal(true)}}><img src="/img/icons/edit.png" alt="edit"></img></div>
-                            {openEditModal && <EditModal closeModal={setOpenEditModal}/>}
-                            <div className={`${styles.iconButton} ${styles.mergeIcon}`} onClick={() => {setOpenMergeModal(true)}}><img src="/img/icons/merge.png" alt="merge"></img></div>
-                            {openMergeModal && <MergeModal closeModal={setOpenEditModal}/>}
-                            <div className={`${styles.iconButton} ${styles.splitIcon}`} onClick={() => {setOpenSplitModal(true)}}><img src="/img/icons/split.png" alt="split"></img></div>
-                            {openSplitModal && (<SplitModal closeModal={setOpenSplitModal} listOfTakers={takersList} courseList={courseList}/>)}
-                            <div className={`${styles.iconButton} ${styles.deleteIcon}`}><img src="/img/icons/trash.png" alt="delete"></img></div>
-                        </div>
-                    </div>
-
-                    {loading ? (
-                        <div className={styles.loading}>Loading...</div>
-                    ) : (
-                      <table>
-                          <thead>
-                              <tr>
-                                  <th></th>
-                                  <th>Course Code</th>
-                                  <th>Course Title</th>
-                                  <th>Offered To</th>
-                                  <th>Sect</th>
-                                  <th>Faculty</th>
-                                  <th>Day 1</th>
-                                  <th>Begin 1</th>
-                                  <th>End 1</th> 
-                                  <th>Room 1</th> 
-                                  <th>Day 2</th>
-                                  <th>Begin 2</th>
-                                  <th>End 2</th> 
-                                  <th>Room 2</th> 
-                                  <th>Enrll Cap</th> 
-                                  <th>Remarks</th> 
-                              </tr>
-                          </thead>
-                          <tbody>
-                              {courseRows}
-                          </tbody>
-                      </table>
-                    )}
+      <div className={styles.container}>
+        <Sidebar/>
+          <div className={styles.content}>
+            <h1>SIS Course Offerings for T3</h1>
+            <div className={styles.tableWrapper}>
+              <div className={styles.controls}>
+                <div className={styles.searchBar}>
+                    <input type="text"/>
                 </div>
-            </div>
+                <div className={styles.iconButtons}>
+                    <div className={`${styles.iconButton} ${styles.addIcon}`}onClick={() => {setOpenAddModal(true)}}><img src="/img/icons/plus.png" alt="add"></img></div>
+                    {openAddModal && <AddModal closeModal={setOpenAddModal}/>}
+                    <div className={`${styles.iconButton} ${styles.editIcon}`} onClick={() => {setOpenEditModal(true)}}><img src="/img/icons/edit.png" alt="edit"></img></div>
+                    {openEditModal && <EditModal closeModal={setOpenEditModal}/>}
+                    <div className={`${styles.iconButton} ${styles.mergeIcon}`} onClick={() => {setOpenMergeModal(true)}}><img src="/img/icons/merge.png" alt="merge"></img></div>
+                    {openMergeModal && <MergeModal closeModal={setOpenEditModal}/>}
+                    <div className={`${styles.iconButton} ${styles.splitIcon}`} onClick={() => {setOpenSplitModal(true)}}><img src="/img/icons/split.png" alt="split"></img></div>
+                    {openSplitModal && (<SplitModal closeModal={setOpenSplitModal} listOfTakers={takersList} courseList={courseList}/>)}
+                    <div className={`${styles.iconButton} ${styles.deleteIcon}`}><img src="/img/icons/trash.png" alt="delete"></img></div>
+                </div>
+              </div>
+
+              { loading ? (<div className={styles.loading }>Loading...</div>) : (
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Course Code</th>
+                            <th>Course Title</th>
+                            <th>Offered To</th>
+                            <th>Sect</th>
+                            <th>Faculty</th>
+                            <th>Day 1</th>
+                            <th>Begin 1</th>
+                            <th>End 1</th> 
+                            <th>Room 1</th> 
+                            <th>Day 2</th>
+                            <th>Begin 2</th>
+                            <th>End 2</th> 
+                            <th>Room 2</th> 
+                            <th>Enrll Cap</th> 
+                            <th>Remarks</th> 
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {courseRows}
+                    </tbody>
+                </table>
+              )}
+          </div>
         </div>
+      </div>
 
     );
 
