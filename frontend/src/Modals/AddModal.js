3c467919e939fdc1a69dbaf8d36152fcefce9831
saac03 <isaac.dicayanan@gmail.com>
2024-08-04 20:36:32 +0800

    modal changes

--- frontend/src/Modals/AddModal.js
+++ frontend/src/Modals/AddModal.js
@@ -8,15 +8,20 @@ function AddModal() {
   const [code, setCode] = useState("");
   const [title, setTitle] = useState("");
   const [section, setSection] = useState("");
+
   const [faculty, setFaculty] = useState("");
+  const [takers, setTakers] = useState("");
+
   const [day1, setDay1] = useState("");
   const [begin1, setBegin1] = useState("");
   const [end1, setEnd1] = useState("");
   const [room1, setRoom1] = useState("");
+
   const [day2, setDay2] = useState("");
   const [begin2, setBegin2] = useState("");
   const [end2, setEnd2] = useState("");
   const [room2, setRoom2] = useState("");
+
   const [enrlCap, setEnrlCap] = useState("");
   const [remarks, setRemarks] = useState("");
   const navigate = useNavigate();
@@ -27,15 +32,20 @@ function AddModal() {
     setCode('');
     setTitle('');
     setSection('');
+
     setFaculty('');
+    setTakers('');
+
     setDay1('');
     setBegin1('');
     setEnd1('');
     setRoom1('');
+
     setDay2('');
     setBegin2('');
     setEnd2('');
     setRoom2('');
+
     setEnrlCap('');
     setRemarks('');
     
@@ -145,17 +155,31 @@ function AddModal() {
             </div>
           </div>
 
-          <div className={styles.formGroup}>
-            <label htmlFor="faculty" className={styles.required}>Faculty</label>
-            <input 
-              type="text" 
-              id="faculty" 
-              className={styles.inputText2}
-              onChange={e => setFaculty(e.target.value)}
-              value={faculty}
-              required
-            />
+          <div className={styles.formRow4}>
+            <div className={styles.formGroup}>
+              <label htmlFor="faculty" className={styles.required}>Faculty</label>
+              <input 
+                type="text" 
+                id="faculty" 
+                className={styles.inputText2}
+                onChange={e => setFaculty(e.target.value)}
+                value={faculty}
+                required
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="takers"className={styles.required}>Takers</label>
+              <input
+                type="number"
+                id="takers"
+                className={styles.inputText2}
+                onChange={e => setTakers(e.target.value)}
+                value={takers}
+                required
+              />
+            </div>
           </div>
+
 
           <div className={styles.formRow}>
             <div className={styles.formGroup}>
@@ -179,6 +203,7 @@ function AddModal() {
             </div>
 
             <div className={styles.formGroup2} id="beginBox1">
+              <label htmlFor="begin1"className={styles.required}>Begin 1</label>
               <select 
                 id="begin1" 
                 name="begin1"
@@ -188,23 +213,17 @@ function AddModal() {
               >
                 <option key="none" value="" disabled></option>
                 <option key="0730" value="0730">0730</option>
-                <option key="0900" value="0900">0900</option>
                 <option key="0915" value="0915">0915</option>
-                <option key="1045" value="1045">1045</option>
                 <option key="1100" value="1100">1100</option>
-                <option key="1230" value="1230">1230</option>
                 <option key="1245" value="1245">1245</option>
-                <option key="1415" value="1415">1415</option>
                 <option key="1430" value="1430">1430</option>
-                <option key="1600" value="1600">1600</option>
                 <option key="1615" value="1615">1615</option>
-                <option key="1745" value="1745">1745</option>
                 <option key="1800" value="1800">1800</option>
-                <option key="1930" value="1930">1930</option>
               </select>
             </div>
 
             <div className={styles.formGroup2}>
+              <label htmlFor="begin2"className={styles.required}>Begin 2</label>
               <select 
                 id="end1" 
                 name="end1"
@@ -213,19 +232,12 @@ function AddModal() {
                 required
               >
                 <option key="none" value="" disabled></option>
-                <option key="0730" value="0730">0730</option>
                 <option key="0900" value="0900">0900</option>
-                <option key="0915" value="0915">0915</option>
                 <option key="1045" value="1045">1045</option>
-                <option key="1100" value="1100">1100</option>
                 <option key="1230" value="1230">1230</option>
-                <option key="1245" value="1245">1245</option>
                 <option key="1415" value="1415">1415</option>
-                <option key="1430" value="1430">1430</option>
                 <option key="1600" value="1600">1600</option>
-                <option key="1615" value="1615">1615</option>
                 <option key="1745" value="1745">1745</option>
-                <option key="1800" value="1800">1800</option>
                 <option key="1930" value="1930">1930</option>
               </select>
             </div>
@@ -264,7 +276,7 @@ function AddModal() {
             </div>
 
             <div className={styles.formGroup2}>
-              <label htmlFor="begin2"></label>
+              <label htmlFor="begin2">Begin 2</label>
               <select 
                 id="begin2"
                 name="day2"
@@ -273,24 +285,17 @@ function AddModal() {
               >
                 <option key="none" value=""></option>
                 <option key="0730" value="0730">0730</option>
-                <option key="0900" value="0900">0900</option>
                 <option key="0915" value="0915">0915</option>
-                <option key="1045" value="1045">1045</option>
                 <option key="1100" value="1100">1100</option>
-                <option key="1230" value="1230">1230</option>
                 <option key="1245" value="1245">1245</option>
-                <option key="1415" value="1415">1415</option>
                 <option key="1430" value="1430">1430</option>
-                <option key="1600" value="1600">1600</option>
                 <option key="1615" value="1615">1615</option>
-                <option key="1745" value="1745">1745</option>
                 <option key="1800" value="1800">1800</option>
-                <option key="1930" value="1930">1930</option>
               </select>
             </div>
 
             <div className={styles.formGroup2}>
-              <label htmlFor="end2"></label>
+              <label htmlFor="end2">End 2</label>
               <select 
                 id="end2"
                 name="end2"
@@ -298,19 +303,12 @@ function AddModal() {
                 value={end2}
               >
                 <option key="none" value=""></option>
-                <option key="0730" value="0730">0730</option>
                 <option key="0900" value="0900">0900</option>
-                <option key="0915" value="0915">0915</option>
                 <option key="1045" value="1045">1045</option>
-                <option key="1100" value="1100">1100</option>
                 <option key="1230" value="1230">1230</option>
-                <option key="1245" value="1245">1245</option>
                 <option key="1415" value="1415">1415</option>
-                <option key="1430" value="1430">1430</option>
                 <option key="1600" value="1600">1600</option>
-                <option key="1615" value="1615">1615</option>
                 <option key="1745" value="1745">1745</option>
-                <option key="1800" value="1800">1800</option>
                 <option key="1930" value="1930">1930</option>
               </select>
             </div>
