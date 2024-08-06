b84c2413c22caafcf3a53037cffa6c0fe63777a1
Nash Luis Maramag <09louisthebob@gmail.com>
2024-08-06 23:45:56 +0800

    Update batchprogram-offerings-controller.js

--- backend/src/controllers/batchprogram-offerings-controller.js
+++ backend/src/controllers/batchprogram-offerings-controller.js
@@ -5,7 +5,6 @@ const CourseOfferings = require('#models/CourseOfferings.js');
 // Get a batch list (or get course page)
 async function getBatchProgramOfferings(req, res) {
   try {
-    console.log(req.originalUrl)
     const { batch, code } = req.query;
     const courseOfferings = await CourseOfferings.find({ takers: { $elemMatch: { batch, programCode: code }}});
     
